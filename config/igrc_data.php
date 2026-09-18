a415d4c5db376f008116b7340dc45fe10c0d2244
emoajib-blip <emoajib@gmail.com>
2026-09-18 10:36:12 +0700

    Add IGRC data and finalize

---
+++ config/igrc_data.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * IGRC 2018 Stunting Detection Data
+ * Indonesian Growth Reference Chart 2018
+ * PLACEHOLDER_HEADER
+ */
+
+$igrc_stunting_cutoffs = [
+PLACEHOLDER_CUTOFFS
+];
+
+/**
+ * PLACEHOLDER_FUNC_DOC
+ */
+function isStunting($age_months, $height_cm, $gender) {
+PLACEHOLDER_FUNC_BODY
+}
+
+/**
+ * PLACEHOLDER_GETCUTOFF_DOC
+ */
+function getStuntingCutoff($age_months, $gender) {
+PLACEHOLDER_GETCUTOFF_BODY
+}
+
+?>
